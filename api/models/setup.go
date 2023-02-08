ca6a11f9bfd1f0e00e5c19c4d04f86c8c00f7830
Kronborg <mkronborg7@gmail.com>
2023-02-08 09:25:21 +0100

    setup file Mirgator

--- api/models/setup.go
+++ api/models/setup.go
@@ -5,5 +5,26 @@ import "gorm.io/gorm"
 func Setup(db *gorm.DB) {
 	db.Migrator().DropTable(
 		&Clubs{},
+		&Months{},
+		&ClubCourts{},
+		&Leaderboards{},
+		&Matchs{},
+		&Results{},
+		&Roles{},
+		&ZipCode{},
+		&UserInfo{},
+		&UserStats{},
+	)
+	db.AutoMigrate(
+		&Clubs{},
+		&Months{},
+		&ClubCourts{},
+		&Leaderboards{},
+		&Matchs{},
+		&Results{},
+		&Roles{},
+		&ZipCode{},
+		&UserInfo{},
+		&UserStats{},
 	)
 }
