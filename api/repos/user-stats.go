550ceda15bee8fbf316f33547126895bf4be9228
Kronborg <mkronborg7@gmail.com>
2023-02-19 02:48:45 +0100

    made FindPlayer repo

--- api/repos/user-stats.go
+++ api/repos/user-stats.go
@@ -6,17 +6,31 @@ import (
 )
 
 type UserStatsRepo struct {
-	repo *gorm.DB
+	db *gorm.DB
 }
 
-func (repo *UserStatsRepo) FindAllPlayerStats() (*[]models.UserStats, error) {
-	return nil, nil
+func (repo *UserStatsRepo) FindAllPlayerStats() ([]models.UserStats, error) {
+	var userStats []models.UserStats
+
+	if err := repo.db.Debug().Find(&userStats).Error; err != nil {
+		return userStats, err
+	}
+	return userStats, nil
 }
 
 func (repo *UserStatsRepo) FindPlayerStats(email string) ([]models.UserStats, error) {
+	var userStats []models.UserStats
+
+	if err := repo.db.Debug().Where("email = ?", email).Find(userStats).Error; err != nil {
+		return userStats, err
+	}
 	return nil, nil
 }
 
 func (repo *UserStatsRepo) UpdatePlayerStats() ([]models.UserStats, error) {
 	return nil, nil
+}
+
+func NewUserStatsRepo(db *gorm.DB) *UserStatsRepo {
+	return &UserStatsRepo{db}
 }
