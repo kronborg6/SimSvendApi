e2835508decfdcf15c206df590c70a375c7f23b2
Kronborg <mkronborg7@gmail.com>
2023-02-21 12:51:32 +0100

    test

--- api/db/db.go
+++ api/db/db.go
@@ -8,6 +8,7 @@ import (
 )
 
 // here we connet to the database useing a conntion string
+// fdgd
 func Init() *gorm.DB {
 	// dsn := "root:Password@tcp(localhost:3306)/simsvend?charset=utf8mb4&parseTime=True&loc=Local" // Home Database
 	dsn := "root:jxgcsTKUUO9OzYYN1Z56@tcp(containers-us-west-191.railway.app:6263)/railway?charset=utf8mb4&parseTime=True&loc=Local" // Home Database
